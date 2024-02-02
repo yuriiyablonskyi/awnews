c1ae8b5fafcfcc27fd3b50cc7f4ea85f4e183697
Yurii <yuriiyablonskyi.i@gmail.com>
2024-02-02 11:16:52 +0100

    u

--- src/store/news/newsActions.ts
+++ src/store/news/newsActions.ts
@@ -1,15 +1,35 @@
-import { createAsyncThunk } from '@reduxjs/toolkit'
+import { createAsyncThunk } from '@reduxjs/toolkit';
 
-const url = 'https://newsapi.org/v2'
-const apiKey = 'apiKey=72310decc144431baadd3552440e662f'
-const request = url + '/everything?q=карто&' + apiKey
+interface SourceArticle {
+  id: string | null;
+  name: string;
+}
+export interface Article {
+  author: string;
+  content: string;
+  description: string;
+  publishedAt: string;
+  source: SourceArticle;
+  title: string;
+  url: string;
+  urlToImage: string | null;
+}
+
+interface FetchNewsResponse {
+  status: string;
+  totalResults: number;
+  articles: Article[];
+}
+
+const url: string = 'https://newsapi.org/v2';
+const apiKey: string = 'apiKey=72310decc144431baadd3552440e662f';
+const request: string = url + '/everything?q=d&' + apiKey;
 
 export const fetchNews = createAsyncThunk(
   'tasks/fetchTasks',
   async () => {
-    // async (param, { rejectWithValue, dispatch }) => {
-    const response = await fetch(request)
-    const data = await response.json()
+    const response = await fetch(request);
+    const data: FetchNewsResponse = await response.json();
     return data.articles
   }
-)
+);
--- src/store/news/newsSlice.ts
+++ src/store/news/newsSlice.ts
@@ -1,28 +1,23 @@
 import { createSlice } from '@reduxjs/toolkit'
 import { fetchNews } from './newsActions'
+import { Article } from './newsActions'
+
+interface NewsState {
+  news: Article; // Замените "YourArticleType" на фактический тип статьи
+  loading: 'idle' | 'pending' | 'failed';
+  error: string | null;
+}
+
+const initialState: NewsState = {
+  news: [],
+  loading: 'idle',
+  error: null,
+};
 
 const newsSlice = createSlice({
   name: 'news',
-  initialState: {
-    news: [],
-    loading: 'idle',
-    error: null
-  },
-
-  reducers: {
-    // addNewTask(state, action) {
-    //   state.tasks.push(action.payload.newTask)
-    // },
-    // updateTaskProperty(state, action) {
-    //   const { id, property, value } = action.payload
-    //   state.tasks = state.tasks.map((task) =>
-    //     task.id === id ? { ...task, [property]: value } : task
-    //   );
-    // },
-    // deleteTask(state, action) {
-    //   state.tasks = state.tasks.filter((task) => task.id !== action.payload.id)
-    // }
-  },
+  initialState,
+  reducers: {},
   extraReducers: builder => {
     builder
       .addCase(fetchNews.pending, (state) => {
@@ -39,12 +34,9 @@ const newsSlice = createSlice({
       .addCase(fetchNews.rejected, (state) => {
         if (state.loading === 'pending') {
           state.loading = 'idle'
-          // state.error = action.payload
         }
       })
   }
 })
-
-// export const { updateTaskProperty, deleteTask, addNewTask } = todosSlice.actions
 
 export default newsSlice.reducer
